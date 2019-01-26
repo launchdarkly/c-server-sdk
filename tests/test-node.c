1b437c8947e224ccc7fce90e153378722d95e635
Harpo Roeder <hroeder@launchdarkly.com>
2019-01-25 17:32:47 -0800

    add encoding / decoding tests for null, bool, text

--- tests/test-node.c
+++ tests/test-node.c
@@ -3,25 +3,93 @@
 void
 allocCheckAndFreeNull()
 {
-    struct LDNode *const node = LDNodeNewNull();
-
+    char *resultJSON = NULL;
+    const char *const expectedJSON = "null";
+    /* construction test */
+    struct LDNode *node = LDNodeNewNull();
     LD_ASSERT(node);
     LD_ASSERT(LDNodeGetType(node) == LDNodeNull);
+    /* encoding test */
+    resultJSON = LDNodeToJSONString(node);
+    LD_ASSERT(strcmp(resultJSON, expectedJSON) == 0);
+    LDNodeFree(node);
+    free(resultJSON);
+    /* decoding test */
+    node = LDNodeFromJSONString("null");
+    LD_ASSERT(node);
+    LD_ASSERT(LDNodeGetType(node) == LDNodeNull);
+    LDNodeFree(node);
+}
 
+void
+allocCheckAndFreeBool()
+{
+    char *resultJSON = NULL;
+    const char *const expectedJSON = "true";
+    const bool expectedValue = true;
+    /* construction test */
+    struct LDNode *node = LDNodeNewBool(expectedValue);
+    LD_ASSERT(node);
+    LD_ASSERT(LDNodeGetType(node) == LDNodeBool);
+    LD_ASSERT(LDNodeGetBool(node) == expectedValue);
+    /* encoding test */
+    resultJSON = LDNodeToJSONString(node);
+    LD_ASSERT(strcmp(resultJSON, expectedJSON) == 0);
+    LDNodeFree(node);
+    free(resultJSON);
+    /* decoding test */
+    node = LDNodeFromJSONString(expectedJSON);
+    LD_ASSERT(node);
+    LD_ASSERT(LDNodeGetType(node) == LDNodeBool);
+    LD_ASSERT(LDNodeGetBool(node) == true);
+    LDNodeFree(node);
+}
+
+void
+allocCheckAndFreeNumber()
+{
+    char *resultJSON = NULL;
+    const char *const expectedJSON = "42";
+    const int expectedValue = 42;
+    /* construction test */
+    struct LDNode *node = LDNodeNewNumber(expectedValue);
+    LD_ASSERT(node);
+    LD_ASSERT(LDNodeGetType(node) == LDNodeNumber);
+    LD_ASSERT(LDNodeGetNumber(node) == expectedValue);
+    /* encoding test */
+    resultJSON = LDNodeToJSONString(node);
+    LD_ASSERT(strcmp(resultJSON, expectedJSON) == 0);
+    LDNodeFree(node);
+    free(resultJSON);
+    /* decoding test */
+    node = LDNodeFromJSONString(expectedJSON);
+    LD_ASSERT(node);
+    LD_ASSERT(LDNodeGetType(node) == LDNodeNumber);
+    LD_ASSERT(LDNodeGetNumber(node) == expectedValue);
     LDNodeFree(node);
 }
 
 void
 allocCheckAndFreeText()
 {
-    const char *const text = "hello world!";
-
-    struct LDNode *const node = LDNodeNewText(text);
-
+    char *resultJSON = NULL;
+    const char *const expectedJSON = "\"hello world!\"";
+    const char *const expectedValue = "hello world!";
+    /* construction test */
+    struct LDNode *node = LDNodeNewText(expectedValue);
     LD_ASSERT(node);
     LD_ASSERT(LDNodeGetType(node) == LDNodeText);
-    LD_ASSERT(strcmp(LDNodeGetText(node), text) == 0);
-
+    LD_ASSERT(strcmp(LDNodeGetText(node), expectedValue) == 0);
+    /* encoding test */
+    resultJSON = LDNodeToJSONString(node);
+    LD_ASSERT(strcmp(resultJSON, expectedJSON) == 0);
+    LDNodeFree(node);
+    free(resultJSON);
+    /* decoding test */
+    node = LDNodeFromJSONString(expectedJSON);
+    LD_ASSERT(node);
+    LD_ASSERT(LDNodeGetType(node) == LDNodeText);
+    LD_ASSERT(strcmp(LDNodeGetText(node), expectedValue) == 0);
     LDNodeFree(node);
 }
 
@@ -31,6 +99,8 @@ main()
     LDConfigureGlobalLogger(LD_LOG_TRACE, LDBasicLogger);
 
     allocCheckAndFreeNull();
+    allocCheckAndFreeBool();
+    allocCheckAndFreeNumber();
     allocCheckAndFreeText();
 
     return 0;
