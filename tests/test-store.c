b6ad092d0b5e151d62f01b2cb06894b29c831e75
Harpo roeder <hroeder@launchdarkly.com>
2019-02-08 11:28:34 -0800

    finish converting upsert tests

--- tests/test-store.c
+++ tests/test-store.c
@@ -132,28 +132,25 @@ upsertNewer()
 
     LDStoreDestroy(store);
 }
-/*
+
 static void
 upsertOlder()
 {
-    struct LDFeatureStore *store = NULL;
-    struct Segment *segment1 = NULL, *segment2 = NULL;
-    struct LDVersionedData *versioned = NULL, *lookup = NULL;
-    struct LDVersionedDataKind kind = getSegmentKind();
+    struct LDStore *store = NULL; struct LDJSON *feature1 = NULL, *feature2 = NULL, *lookup = NULL;
 
     LD_ASSERT(store = prepareEmptyStore());
 
-    LD_ASSERT(segment1 = constructSegment("my-heap-key", 5))
-    LD_ASSERT(versioned = segmentToVersioned(segment1));
-    LD_ASSERT(store->upsert(store->context, kind, versioned));
+    LD_ASSERT(feature1 = makeVersioned("my-heap-key", 5))
+    LD_ASSERT(LDStoreUpsert(store, "segments", feature1));
 
-    LD_ASSERT(segment2 = constructSegment("my-heap-key", 3))
-    LD_ASSERT(versioned = segmentToVersioned(segment2));
-    LD_ASSERT(store->upsert(store->context, kind, versioned));
+    LD_ASSERT(feature2 = makeVersioned("my-heap-key", 3))
+    LD_ASSERT(LDStoreUpsert(store, "segments", feature2));
 
-    LD_ASSERT((lookup = store->get(store->context, "my-heap-key", kind)));
-    LD_ASSERT(lookup->data == segment1);
-    store->finalizeGet(store->context, lookup);
+    LD_ASSERT((lookup = LDStoreGet(store, "my-heap-key", "segments")));
+
+    /* LD_ASSERT(lookup->data == segment2); requires deep compare */
+
+    LDJSONFree(lookup);
 
     LDStoreDestroy(store);
 }
@@ -161,27 +158,22 @@ upsertOlder()
 static void
 upsertDelete()
 {
-    struct LDFeatureStore *store = NULL;
-    struct Segment *segment1 = NULL, *segment2 = NULL;
-    struct LDVersionedData *versioned = NULL, *lookup = NULL;
-    struct LDVersionedDataKind kind = getSegmentKind();
+    struct LDStore *store = NULL; struct LDJSON *feature = NULL, *lookup = NULL;
 
     LD_ASSERT(store = prepareEmptyStore());
 
-    LD_ASSERT(segment1 = constructSegment("my-heap-key", 8))
-    LD_ASSERT(versioned = segmentToVersioned(segment1));
-    LD_ASSERT(store->upsert(store->context, kind, versioned));
+    LD_ASSERT(feature = makeVersioned("my-heap-key", 3))
+    LD_ASSERT(LDStoreUpsert(store, "segments", feature));
 
-    LD_ASSERT(segment2 = segmentMakeDeleted("my-heap-key", 10))
-    LD_ASSERT(versioned = segmentToVersioned(segment2));
-    LD_ASSERT(store->upsert(store->context, kind, versioned));
+    LD_ASSERT(feature = makeDeleted("my-heap-key", 5))
+    LD_ASSERT(LDStoreUpsert(store, "segments", feature));
 
-    LD_ASSERT(!(lookup = store->get(store->context, "my-heap-key", kind)));
-    store->finalizeGet(store->context, lookup);
+    LD_ASSERT(!(lookup = LDStoreGet(store, "my-heap-key", "segments")));
 
     LDStoreDestroy(store);
 }
 
+/*
 static void
 conflictDifferentNamespace()
 {
@@ -223,10 +215,10 @@ main()
     basicExists();
     basicDoesNotExist();
     upsertNewer();
-
-    /*
     upsertOlder();
     upsertDelete();
+
+    /*
     conflictDifferentNamespace();
     */
 
