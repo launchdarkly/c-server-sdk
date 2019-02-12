143803f774cf8311c0e917183fd3d1d9be105039
Harpo Roeder <hroeder@launchdarkly.com>
2019-02-12 10:39:23 -0800

    partially added error channel to evaluation

--- src/ldevaluate.c
+++ src/ldevaluate.c
@@ -13,13 +13,18 @@ static bool ruleMatchesUser(const struct LDJSON *const rule, const struct LDUser
 
 static bool clauseMatchesUser(const struct LDJSON *const clause, const struct LDUser *const user);
 
-bool segmentMatchesUser(const struct LDJSON *const segment, const struct LDUser *const user);
+bool segmentMatchesUser(const struct LDJSON *const segment,
+    const struct LDUser *const user, bool *const matches);
 
-bool segmentRuleMatchUser(const struct LDJSON *const segmentRule, const char *const segmentKey, const struct LDUser *const user, const char *const salt);
+bool segmentRuleMatchUser(const struct LDJSON *const segmentRule,
+    const char *const segmentKey, const struct LDUser *const user,
+    const char *const salt, bool *const matches);
 
-bool clauseMatchesUserNoSegments(const struct LDJSON *const clause, const struct LDUser *const user);
+bool clauseMatchesUserNoSegments(const struct LDJSON *const clause,
+    const struct LDUser *const user, bool *const matches);
 
-float bucketUser(const struct LDUser *const user, const char *const segmentKey, const char *const attribute, const char *const salt);
+bool bucketUser(const struct LDUser *const user, const char *const segmentKey,
+    const char *const attribute, const char *const salt, float *const bucket);
 
 static char *bucketableStringValue(const struct LDJSON *const node);
 
@@ -28,12 +33,15 @@ static char *bucketableStringValue(const struct LDJSON *const node);
 bool
 evaluate(const struct LDJSON *const flag, const struct LDUser *const user)
 {
-    LD_ASSERT(flag); LD_ASSERT(user); LD_ASSERT(LDJSONGetType(flag) == LDObject);
+    LD_ASSERT(flag);
+    LD_ASSERT(user);
+    LD_ASSERT(LDJSONGetType(flag) == LDObject);
 
     {
         const struct LDJSON *on = LDObjectLookup(flag, "on");
 
-        LD_ASSERT(on); LD_ASSERT(LDJSONGetType(on) == LDBool);
+        LD_ASSERT(on);
+        LD_ASSERT(LDJSONGetType(on) == LDBool);
 
         if (!LDGetBool(on)) {
             /* TODO return isOFf */
@@ -94,7 +102,9 @@ checkPrerequisites(const struct LDJSON *const flag, const struct LDUser *const u
 {
     struct LDJSON *prerequisites = NULL, *iter = NULL;
 
-    LD_ASSERT(flag); LD_ASSERT(LDJSONGetType(flag) == LDObject); LD_ASSERT(user);
+    LD_ASSERT(flag);
+    LD_ASSERT(LDJSONGetType(flag) == LDObject);
+    LD_ASSERT(user);
 
     LD_ASSERT(prerequisites = LDObjectLookup(flag, "prerequisites"));
 
@@ -117,7 +127,8 @@ ruleMatchesUser(const struct LDJSON *const rule, const struct LDUser *const user
 {
     const struct LDJSON *clauses = NULL, *iter = NULL;
 
-    LD_ASSERT(rule); LD_ASSERT(user);
+    LD_ASSERT(rule);
+    LD_ASSERT(user);
 
     LD_ASSERT(clauses = LDObjectLookup(rule, "clauses"));
 
@@ -139,9 +150,12 @@ clauseMatchesUser(const struct LDJSON *const clause, const struct LDUser *const 
 {
     const struct LDJSON *op = NULL;
 
-    LD_ASSERT(clause); LD_ASSERT(LDJSONGetType(clause) == LDObject); LD_ASSERT(user);
+    LD_ASSERT(clause);
+    LD_ASSERT(LDJSONGetType(clause) == LDObject);
+    LD_ASSERT(user);
 
-    LD_ASSERT(op = LDObjectLookup(clause, "op")); LD_ASSERT(LDJSONGetType(op) == LDText);
+    LD_ASSERT(op = LDObjectLookup(clause, "op"));
+    LD_ASSERT(LDJSONGetType(op) == LDText);
 
     if (strcmp(LDGetText(op), "segmentMatch") == 0) {
         const struct LDJSON *values = NULL, *iter = NULL, *negate = NULL;
@@ -171,64 +185,143 @@ clauseMatchesUser(const struct LDJSON *const clause, const struct LDUser *const 
 }
 
 bool
-segmentMatchesUser(const struct LDJSON *const segment, const struct LDUser *const user)
+segmentMatchesUser(const struct LDJSON *const segment,
+    const struct LDUser *const user, bool *const matches)
 {
-    const struct LDJSON *included = NULL, *excluded = NULL;
+    const struct LDJSON *included = NULL;
+    const struct LDJSON *excluded = NULL;
 
-    LD_ASSERT(segment); LD_ASSERT(user);
+    LD_ASSERT(segment);
+    LD_ASSERT(user);
 
-    LD_ASSERT(included = LDObjectLookup(segment, "included"));
+    if (!(included = LDObjectLookup(segment, "included"))) {
+        LDi_log(LD_LOG_ERROR, "schema error");
 
-    LD_ASSERT(LDJSONGetType(included) == LDArray);
+        return false;
+    }
+
+    if (LDJSONGetType(included) != LDArray) {
+        LDi_log(LD_LOG_ERROR, "schema error");
+
+        return false;
+    }
 
     if (textInArray(included, user->key)) {
+        *matches = true;
+
         return true;
     } else if (textInArray(excluded, user->key)) {
-        return false;
+        *matches = false;
+
+        return true;
     } else {
-        const struct LDJSON *segmentRules = NULL, *iter = NULL;
+        const struct LDJSON *segmentRules = NULL;
+        const struct LDJSON *iter = NULL;
 
-        LD_ASSERT(segmentRules = LDObjectLookup(segment, "rules"));
+        if (!(segmentRules = LDObjectLookup(segment, "included"))) {
+            LDi_log(LD_LOG_ERROR, "schema error");
 
-        LD_ASSERT(LDJSONGetType(segmentRules) == LDArray);
+            return false;
+        }
+
+        if (LDJSONGetType(segmentRules) != LDArray) {
+            LDi_log(LD_LOG_ERROR, "schema error");
+
+            return false;
+        }
 
         for (iter = LDGetIter(segmentRules); iter; iter = LDIterNext(iter)) {
-            const struct LDJSON *key = NULL, *salt = NULL;
+            const struct LDJSON *key = NULL;
+            const struct LDJSON *salt = NULL;
+            bool submatches;
 
-            LD_ASSERT(LDJSONGetType(iter) == LDObject);
+            if (LDJSONGetType(iter) != LDObject) {
+                LDi_log(LD_LOG_ERROR, "schema error");
 
-            LD_ASSERT(key = LDObjectLookup(iter, "key"));
+                return false;
+            }
 
-            LD_ASSERT(LDJSONGetType(key) == LDText);
+            if (!(key = LDObjectLookup(iter, "key"))) {
+                LDi_log(LD_LOG_ERROR, "schema error");
 
-            LD_ASSERT(salt = LDObjectLookup(iter, "salt"));
+                return false;
+            }
 
-            LD_ASSERT(LDJSONGetType(salt) == LDText);
+            if (LDJSONGetType(key) != LDText) {
+                LDi_log(LD_LOG_ERROR, "schema error");
 
-            if (segmentRuleMatchUser(iter, LDGetText(key), user, LDGetText(salt))) {
+                return false;
+            }
+
+            if (!(salt = LDObjectLookup(iter, "salt"))) {
+                LDi_log(LD_LOG_ERROR, "schema error");
+
+                return false;
+            }
+
+            if (LDJSONGetType(salt) != LDText) {
+                LDi_log(LD_LOG_ERROR, "schema error");
+
+                return false;
+            }
+
+            if (!segmentRuleMatchUser(iter, LDGetText(key), user,
+                LDGetText(salt), &submatches))
+            {
+                return false;
+            }
+
+            if (submatches) {
+                *matches = submatches;
+
                 return true;
             }
         }
 
-        return false;
+        *matches = false;
+
+        return true;
     }
 }
 
 bool
-segmentRuleMatchUser(const struct LDJSON *const segmentRule, const char *const segmentKey, const struct LDUser *const user, const char *const salt)
+segmentRuleMatchUser(const struct LDJSON *const segmentRule,
+    const char *const segmentKey, const struct LDUser *const user,
+    const char *const salt, bool *const matches)
 {
-    LD_ASSERT(segmentRule); LD_ASSERT(segmentKey); LD_ASSERT(user); LD_ASSERT(salt);
+    LD_ASSERT(segmentRule);
+    LD_ASSERT(segmentKey);
+    LD_ASSERT(user);
+    LD_ASSERT(salt);
+    LD_ASSERT(matches);
 
     {
-        const struct LDJSON *clauses = NULL, *clause = NULL;
+        const struct LDJSON *clauses = NULL;
+        const struct LDJSON *clause = NULL;
 
-        LD_ASSERT(clauses = LDObjectLookup(segmentRule, "clauses"))
+        if (!(clauses = LDObjectLookup(segmentRule, "clauses"))) {
+            LDi_log(LD_LOG_ERROR, "schema error");
 
-        LD_ASSERT(LDJSONGetType(clauses) == LDArray);
+            return false;
+        }
+
+        if (LDJSONGetType(clauses) != LDArray) {
+            LDi_log(LD_LOG_ERROR, "schema error");
+
+            return false;
+        }
 
         for (clause = LDGetIter(clauses); clause; clause = LDIterNext(clause)) {
-            if (!clauseMatchesUserNoSegments(clause, user)) {
+            bool submatches;
+
+            if (!clauseMatchesUserNoSegments(clause, user, &submatches)) {
                 return false;
+            }
+
+            if (!submatches) {
+                *matches = false;
+
+                return true;
             }
         }
     }
@@ -240,101 +333,161 @@ segmentRuleMatchUser(const struct LDJSON *const segmentRule, const char *const s
             return true;
         }
 
-        LD_ASSERT(LDJSONGetType(weight) == LDNumber);
+        if (LDJSONGetType(weight) != LDNumber) {
+            LDi_log(LD_LOG_ERROR, "schema error");
+
+            return false;
+        }
 
         {
-            const struct LDJSON *bucketBy = LDObjectLookup(segmentRule, "bucketBy");
+            float bucket;
 
-            const char *const attribute = bucketBy == NULL ? "key" : LDGetText(bucketBy);
+            const struct LDJSON *bucketBy =
+                LDObjectLookup(segmentRule, "bucketBy");
 
-            return bucketUser(user, segmentKey, attribute, salt) < LDGetNumber(weight) / 100000;
+            const char *const attribute = (bucketBy == NULL)
+                ? "key" : LDGetText(bucketBy);
+
+            if (!bucketUser(user, segmentKey, attribute, salt, &bucket)) {
+                LDi_log(LD_LOG_ERROR, "bucketUser error");
+
+                return false;
+            }
+
+            *matches = bucket < LDGetNumber(weight) / 100000;
+
+            return true;
         }
     }
 }
 
 bool
-clauseMatchesUserNoSegments(const struct LDJSON *const clause, const struct LDUser *const user)
+clauseMatchesUserNoSegments(const struct LDJSON *const clause,
+    const struct LDUser *const user, bool *const matches)
 {
-    struct LDJSON *attributeValue = NULL, *attribute = NULL;
+    const char *attributeText = NULL;
+    struct LDJSON *attributeValue = NULL;
+    struct LDJSON *attribute = NULL;
+    LDJSONType type;
 
-    LD_ASSERT(clause); LD_ASSERT(user);
+    LD_ASSERT(clause);
+    LD_ASSERT(user);
 
-    LD_ASSERT(attribute = LDObjectLookup(clause, "attribute"));
+    if (!(attribute = LDObjectLookup(clause, "attribute"))) {
+        LDi_log(LD_LOG_ERROR, "schema error");
 
-    LD_ASSERT(LDJSONGetType(attribute) == LDText);
+        return false;
+    }
 
-    if ((attributeValue = valueOfAttribute(user, LDGetText(attribute)))) {
-        LDJSONType type = LDJSONGetType(attributeValue);
+    if (LDJSONGetType(attribute) != LDText) {
+        LDi_log(LD_LOG_ERROR, "schema error");
 
-        if (type == LDArray) {
-            struct LDJSON *iter = NULL;
+        return false;
+    }
 
-            for (iter = LDGetIter(attributeValue); iter; iter = LDIterNext(iter)) {
-                type = LDJSONGetType(iter);
+    if (!(attributeText = LDGetText(attribute))) {
+        LDi_log(LD_LOG_ERROR, "allocation error");
 
-                if (type == LDObject || type == LDArray) {
-                    LDJSONFree(attributeValue);
+        return false;
+    }
 
-                    return false;
-                }
+    if (!(attributeValue = valueOfAttribute(user, attributeText))) {
+        LDi_log(LD_LOG_ERROR, "schema error");
+
+        return false;
+    }
+
+    if ((type = LDJSONGetType(attributeValue)) == LDArray) {
+        const struct LDJSON *negate = NULL;
+        struct LDJSON *iter = LDGetIter(attributeValue);
+
+        for (; iter; iter = LDIterNext(iter)) {
+            type = LDJSONGetType(iter);
+
+            if (type == LDObject || type == LDArray) {
+                LDi_log(LD_LOG_ERROR, "schema error");
+
+                LDJSONFree(attributeValue);
+
+                return false;
             }
+        }
 
-            LDJSONFree(attributeValue);
+        LDJSONFree(attributeValue);
 
-            {
-                const struct LDJSON *const negate = LDObjectLookup(clause, "negate");
-
-                LD_ASSERT(negate); LD_ASSERT(LDJSONGetType(negate) == LDBool);
-
-                return LDGetBool(negate) ? false : true;
-            }
-        } else if (type != LDObject && type != LDArray) {
-            LDJSONFree(attributeValue);
-
-            return true;
-        } else {
-            LDJSONFree(attributeValue);
+        if (!(negate = LDObjectLookup(clause, "negate"))) {
+            LDi_log(LD_LOG_ERROR, "schema error");
 
             return false;
         }
+
+        if (LDJSONGetType(negate) != LDBool) {
+            LDi_log(LD_LOG_ERROR, "schema error");
+
+            return false;
+        }
+
+        *matches = LDGetBool(negate) ? false : true;
+
+        return true;
+    } else if (type != LDObject && type != LDArray) {
+
+        /* match any */
+        LDJSONFree(attributeValue);
+
+        return true;
     } else {
+        LDJSONFree(attributeValue);
+
         return false;
     }
 }
 
-float
-bucketUser(const struct LDUser *const user, const char *const segmentKey, const char *const attribute, const char *const salt)
+bool
+bucketUser(const struct LDUser *const user, const char *const segmentKey,
+    const char *const attribute, const char *const salt, float *const bucket)
 {
     struct LDJSON *attributeValue = NULL;
 
-    LD_ASSERT(user); LD_ASSERT(segmentKey); LD_ASSERT(attribute); LD_ASSERT(salt);
+    LD_ASSERT(user);
+    LD_ASSERT(segmentKey);
+    LD_ASSERT(attribute);
+    LD_ASSERT(salt);
+    LD_ASSERT(bucket);
 
     if ((attributeValue = valueOfAttribute(user, attribute))) {
-        const char *const bucketable = bucketableStringValue(attributeValue);
+        char raw[256];
+
+        char *const bucketable = bucketableStringValue(attributeValue);
 
         LDJSONFree(attributeValue);
 
-        if (bucketable) {
-            char raw[256];
-
-            if (snprintf(raw, sizeof(raw), "%s.%s.%s", segmentKey, salt, bucketable) < 0) {
-                return 0;
-            } else {
-                char digest[20], encoded[17]; float longScale = 0xFFFFFFFFFFFFFFF;
-
-                SHA1(digest, raw, strlen(raw));
-
-                /* encodes to hex, and shortens, 16 characters in hex == 8 bytes */
-                LD_ASSERT(hexify((unsigned char *)digest, sizeof(digest), encoded, sizeof(encoded)) == 16);
-
-                return (float)strtoll(encoded, NULL, 16) / longScale;
-            }
-        } else {
-            return 0;
+        if (!bucketable) {
+            return false;
         }
-    } else {
-        return 0;
+
+        if (snprintf(raw, sizeof(raw), "%s.%s.%s", segmentKey,
+            salt, bucketable) >= 0)
+        {
+            char digest[20];
+            char encoded[17];
+            float longScale = 0xFFFFFFFFFFFFFFF;
+
+            SHA1(digest, raw, strlen(raw));
+
+            /* encodes to hex, and shortens, 16 characters in hex 8 bytes */
+            LD_ASSERT(hexify((unsigned char *)digest,
+                sizeof(digest), encoded, sizeof(encoded)) == 16);
+
+            *bucket = (float)strtoll(encoded, NULL, 16) / longScale;
+
+            free(bucketable);
+
+            return true;
+        }
     }
+
+    return false;
 }
 
 static char *
@@ -347,7 +500,7 @@ bucketableStringValue(const struct LDJSON *const node)
     } else if (LDJSONGetType(node) == LDNumber) {
         char buffer[256];
 
-        if (snprintf(buffer, sizeof(buffer), "%d", (int)LDGetNumber(node)) < 0) {
+        if (snprintf(buffer, sizeof(buffer), "%f", LDGetNumber(node)) < 0) {
             return NULL;
         } else {
             return strdup(buffer);
