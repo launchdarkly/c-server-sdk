fdc775dba61e63f0eb380c5cbe1c2b02eac42525
Harpo Roeder <hroeder@launchdarkly.com>
2019-03-06 21:34:02 +0000

    add testAllFlagsReturnsNilIfUserKeyIsNil()

--- tests/test-allflags.c
+++ tests/test-allflags.c
@@ -88,6 +88,45 @@ testAllFlags()
     LDClientClose(client);
 }
 
+static void
+testAllFlagsReturnsNilIfUserKeyIsNil()
+{
+    struct LDJSON *flag1;
+    struct LDJSON *flag2;
+    struct LDClient *client;
+
+    LD_ASSERT(client = makeTestClient());
+
+    /* flag1 */
+    LD_ASSERT(flag1 = LDNewObject());
+    LD_ASSERT(LDObjectSetKey(flag1, "key", LDNewText("flag1")));
+    LD_ASSERT(LDObjectSetKey(flag1, "version", LDNewNumber(1)));
+    LD_ASSERT(LDObjectSetKey(flag1, "on", LDNewBool(true)));
+    setFallthrough(flag1, 1);
+    addVariation(flag1, LDNewText("a"));
+    addVariation(flag1, LDNewText("b"));
+
+    /* flag2 */
+    LD_ASSERT(flag2 = LDNewObject());
+    LD_ASSERT(LDObjectSetKey(flag2, "key", LDNewText("flag2")));
+    LD_ASSERT(LDObjectSetKey(flag2, "version", LDNewNumber(1)));
+    LD_ASSERT(LDObjectSetKey(flag2, "on", LDNewBool(true)));
+    setFallthrough(flag2, 1);
+    addVariation(flag2, LDNewText("c"));
+    addVariation(flag2, LDNewText("d"));
+
+    /* store */
+    LD_ASSERT(LDStoreInitEmpty(client->config->store));
+    LD_ASSERT(LDStoreUpsert(client->config->store, "flags", flag1));
+    LD_ASSERT(LDStoreUpsert(client->config->store, "flags", flag2));
+
+    /* test / validation */
+    LD_ASSERT(!LDAllFlags(client, NULL));
+
+    /* cleanup */
+    LDClientClose(client);
+}
+
 int
 main()
 {
@@ -95,6 +134,7 @@ main()
     LDGlobalInit();
 
     testAllFlags();
+    testAllFlagsReturnsNilIfUserKeyIsNil();
 
     return 0;
 };
