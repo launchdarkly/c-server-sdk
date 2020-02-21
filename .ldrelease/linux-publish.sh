2053a20388461697806b7efc1be1754cfa7bd347
David Calavera <david.calavera@gmail.com>
2020-02-21 14:24:26 -0800

    Fix dynamic object name in release artifact
    Otherwise, when the linker tries to load the object in runtime you get an error like this if you don't rename the file manually:
    
    ```
     - libldserverapidynamic.so: cannot open shared object file: No such file or directory
    ```

--- .ldrelease/linux-publish.sh
+++ .ldrelease/linux-publish.sh
@@ -1,5 +1,5 @@
 mkdir -p artifacts
 
 cp build/libldserverapi.a artifacts/${LD_LIBRARY_FILE_PREFIX}-libldserverapi.a
-cp build/libldserverapidynamic.so artifacts/${LD_LIBRARY_FILE_PREFIX}-libldserverapi.so
+cp build/libldserverapidynamic.so artifacts/${LD_LIBRARY_FILE_PREFIX}-libldserverapidynamic.so
 cp build/stores/redis/libldserverapi-redis.a artifacts/${LD_LIBRARY_FILE_PREFIX}-libldserverapi-redis.a
