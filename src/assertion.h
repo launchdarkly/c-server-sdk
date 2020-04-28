ac1422e313bcf8e8309956929879e82855fd22a3
hroederld <hroeder@launchdarkly.com>
2020-04-28 10:35:59 -0700

    [ch73853] enable defensive api by default (#81)

--- src/assertion.h
+++ src/assertion.h
@@ -15,9 +15,12 @@
             LD_LOG(LD_LOG_FATAL, "LD_ASSERT failed: " #condition " aborting"); \
             abort(); \
         }
-
-    #define LD_ASSERT_API(condition) LD_ASSERT(condition)
 #else
     #define LD_ASSERT(condition)
+#endif
+
+#ifdef LAUNCHDARKLY_OFFENSIVE
+    #define LD_ASSERT_API(condition) LD_ASSERT(condition)
+#else
     #define LD_ASSERT_API(condition)
 #endif
